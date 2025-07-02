f3ac482e2a0d99d7e22313285e6809d7cae0a201
Tharun Kumaar <tharunkumaargslearn@gmail.com>
2025-07-02 23:34:44 +0530

    Update quality_check_silver.sql

--- tests/quality_check_silver.sql
+++ tests/quality_check_silver.sql
@@ -30,17 +30,17 @@ FROM silver.crm_cust_info
 GROUP BY cst_id
 HAVING COUNT(*) > 1 OR cst_id IS NULL;
 
+-- Data Standardization & Consistency
+SELECT DISTINCT 
+    cst_marital_status 
+FROM silver.crm_cust_info;
+
 -- Check for Unwanted Spaces
 -- Expectation: No Results
 SELECT 
     cst_key 
 FROM silver.crm_cust_info
 WHERE cst_key != TRIM(cst_key);
-
--- Data Standardization & Consistency
-SELECT DISTINCT 
-    cst_marital_status 
-FROM silver.crm_cust_info;
 
 -- ====================================================================
 -- Checking 'silver.crm_prd_info'
@@ -53,6 +53,11 @@ SELECT
 FROM silver.crm_prd_info
 GROUP BY prd_id
 HAVING COUNT(*) > 1 OR prd_id IS NULL;
+
+-- Data Standardization & Consistency
+SELECT DISTINCT 
+    prd_line 
+FROM silver.crm_prd_info;
 
 -- Check for Unwanted Spaces
 -- Expectation: No Results
@@ -68,10 +73,6 @@ SELECT
 FROM silver.crm_prd_info
 WHERE prd_cost < 0 OR prd_cost IS NULL;
 
--- Data Standardization & Consistency
-SELECT DISTINCT 
-    prd_line 
-FROM silver.crm_prd_info;
 
 -- Check for Invalid Date Orders (Start Date > End Date)
 -- Expectation: No Results
